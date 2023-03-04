76866a205f0b8fc7c2edc2a0848075d5592c423e
Shogo Goto <gotoadmn0605@gmail.com>
2023-03-04 16:45:33 +0900

    spaceでもCDできるよう変更

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from . import features as F
-
+from src import features as F
 
 app = FastAPI()
 app.include_router(F.concept.api.router)
@@ -9,7 +8,6 @@ app.include_router(F.concept.api.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World2"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from .api import app
+from . import features
