cc52279e3b09239cfd333e9eb85a3e1a22800e53
Shogo Goto <gotoadmn0605@gmail.com>
2023-04-08 15:56:53 +0900

    コミット忘れ

---
+++ src/features/concept/repo/cypher/test_result.py
@@ -0,0 +1,15 @@
+from ..test_nx_repo import narrow_tree
+from . import result as R
+
+def test_getattr_for_resolved_result(narrow_tree):
+    g, _ = narrow_tree
+    result = R.ResolvedResult(g.nodes)
+    assert result.uids == set([r.uid for r in g.nodes])
+    assert result.names == set([r.name for r in g.nodes])
+    assert result.unknown_keys == set()
+
+def test_getattr_empty():
+    empty = R.ResolvedResult([])
+
+    assert empty.uids == set()
+    assert empty.unkwons == set()
