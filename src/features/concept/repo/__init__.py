3cb7b44d6e6248c91b739fe4d7b561fe8f7d68b3
Shogo Goto <gotoadmn0605@gmail.com>
2023-04-17 11:27:59 +0900

    ConceptLabelをファイル分離

--- src/features/concept/repo/__init__.py
+++ src/features/concept/repo/__init__.py
@@ -1,21 +1,7 @@
-from neomodel import (
-        StructuredNode,
-        UniqueIdProperty,
-        StringProperty,
-        RelationshipFrom,
-        RelationshipTo,
-        db,
-        Relationship
-        )
-
-
-REL_TYPE: str = "INFER"
-
-class Concept(StructuredNode):
-    uid         = UniqueIdProperty()
-    name        = StringProperty()
-    description = StringProperty()
-    # sourceは既に使われているプロパティ名っぽい
-    srcs        = RelationshipFrom("Concept", REL_TYPE)
-    dests       = RelationshipTo("Concept", REL_TYPE)
-
+from .label import Concept
+from .node_crud import (
+    ConceptCommand
+    , UidQuery
+    )
+from .rel_crud import InferRepo
+from .stats_query import WithStatisticsQuery
